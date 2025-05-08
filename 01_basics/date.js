36319b88c4df3324e6174568701a7f1697929806
Kison-Khadka <kison914@gmail.com>
2025-05-09 01:06:07 +0545

    Add

---
+++ 01_basics/date.js
@@ -0,0 +1,37 @@
+
+
+const myDate = new Date()
+
+// console.log(myDate);
+
+console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+
+console.log(myDate.toLocaleDateString());
+
+console.log(myDate.toLocaleString());
+
+console.log(myDate.toLocaleTimeString());
+
+console.log(myDate.toTimeString());
+
+console.log(myDate.toUTCString());
+
+// console.log(myDate.getTimezoneOffset());
+
+console.log(myDate.getHours());
+
+console.log(myDate.getDate());
+
+
+console.log(typeof myDate);
+
+const myCreateDate = new Date(2025-3-4)
+console.log(myCreateDate.toDateString());
+
+
+
+
